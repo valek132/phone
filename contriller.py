ba4dd1508d9b0e58bf8b8f0530164281512418e8
Valek <pasport96@gmail.com>
2023-01-24 13:01:27 +0300

    Initial commit

---
+++ contriller.py
@@ -0,0 +1,32 @@
+import add_contact as ac
+import user_interface as ui
+import change_phone_number as cpn
+import change_surname as cs
+import delete_contact as dc
+import view_all_contacts as vac
+import export_in_file as eif
+
+
+def user_choice():
+
+    choice_num = ui.menu()
+    if choice_num < 0 or choice_num > 7:
+        print('\nОшибка ввода!\n\nЧисло должно соответствовать пункту меню!\n')
+        user_choice()
+    elif choice_num == 0:
+        ac.create_json()
+    elif choice_num == 1:
+        ac.add_to_json()
+    elif choice_num == 2:
+        cpn.change_phone_number()
+    elif choice_num == 3:
+        cs.change_surname()
+    elif choice_num == 4:
+        dc.delete_contact()
+    elif choice_num == 5:
+        vac.view_all_contacts()
+    elif choice_num == 6:
+        eif.export_txt()
+    elif choice_num == 7:
+        print('\n Досвидания')
+        exit()
---
+++ main.py
@@ -0,0 +1,5 @@
+import contriller
+import user_interface as ui
+
+ui.start()
+contriller.user_choice()
